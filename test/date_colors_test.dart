79d51191b677a05bfa5ca1377a981065b939f963
eightgran <eightgran@gmail.com>
2022-09-01 18:25:26 +0200

    Add tests for `ColorsOfTheYear` - Tests the implementation according current specifications

--- test/date_colors_test.dart
+++ test/date_colors_test.dart
@@ -3,10 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:date_colors/date_colors.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('UT-COTH01-VerifiesWhetherAllColorsArePresent', () {
+    final int mapLength = ColorsOfTheYear.length;
+    expect(mapLength, 10);
+  });
+  test('UT-COTH02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
+    final int? color = ColorsOfTheYear.values[0];
+    expect(color, 0xFF6c6874);
   });
 }
