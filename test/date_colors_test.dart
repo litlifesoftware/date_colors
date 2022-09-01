0242287034d76a3714b4de5efb60ec52edb7c9f4
eightgran <eightgran@gmail.com>
2022-09-01 20:18:21 +0200

    Add `ColorsOfFebruary` - Contains color values for the month of February - Implement tests

--- test/date_colors_test.dart
+++ test/date_colors_test.dart
@@ -1,25 +1,35 @@
-import 'package:date_colors/src/data/colors_of_january.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:date_colors/date_colors.dart';
 
 void main() {
   // Unit Test: `ColorsOfTheYear`
-  test('UT-COTH01-VerifiesWhetherAllColorsArePresent', () {
+  test('UT-COTY01-VerifiesWhetherAllColorsArePresent', () {
     final int mapLength = ColorsOfTheYear.length;
     expect(mapLength, 10);
   });
-  test('UT-COTH02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
+  test('UT-COTY02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
     final int? color = ColorsOfTheYear.values[0];
     expect(color, 0xFF6c6874);
   });
+
   // Unit Test: `ColorsOfJanuary`
-  test('UT-COJ01-VerifiesWhetherAllColorsArePresent', () {
+  test('UT-COJan01-VerifiesWhetherAllColorsArePresent', () {
     final int mapLength = ColorsOfJanuary.length;
     expect(mapLength, 31);
   });
-  test('UT-COTH02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
+  test('UT-COJan02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
     final int? color = ColorsOfJanuary.values[1];
     expect(color, 0xFF015696);
+  });
+
+  // Unit Test: `ColorsOfFebruary`
+  test('UT-COFeb01-VerifiesWhetherAllColorsArePresent', () {
+    final int mapLength = ColorsOfFebruary.length;
+    expect(mapLength, 29);
+  });
+  test('UT-COFeb02-VerifiesWhetherTheFirstColorMatchesSpecifications', () {
+    final int? color = ColorsOfFebruary.values[1];
+    expect(color, 0xFFC60650);
   });
 }
