d7750c58af104e7512ceaf3f1bdde9ded8e999f8
eightgran <eightgran@gmail.com>
2022-09-12 16:40:54 +0200

    Add Homescreen tabs on example app

--- example/lib/src/screens/home_screen.dart
+++ example/lib/src/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:example/src/screens.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,12 +25,8 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   List<Widget> screenTabContent = const [
-    Center(
-      child: Text("Months"),
-    ),
-    Center(
-      child: Text("Picker"),
-    ),
+    MonthScreen(),
+    PickerScreen(),
   ];
 
   @override
---
+++ example/lib/src/screens/month_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MonthScreen extends StatelessWidget {
+  const MonthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Months"),
+    );
+  }
+}
