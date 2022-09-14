aafd820ea5acd1c8f8fa9f786a0aaa4a7904d26a
eightgran <eightgran@gmail.com>
2022-09-14 21:01:11 +0200

    Create basic example app test - Verifies the tab view transition

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,26 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('MonthsScreenTest', (WidgetTester tester) async {
+  testWidgets('EX-1-HomeScreen-Tab-Transition', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const App());
 
-    // Verify that our counter starts at 0.
+    // Verify that the Months Screen is present.
     expect(find.text('Months'), findsWidgets);
-    // expect(find.text('1'), findsNothing);
+    // Verify that the Picker Screen is present.
+    expect(find.text('Picker'), findsOneWidget);
+    // Verify that the first January List Tile is present.
+    expect(find.text('January'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    // Tap the Picker Navigation Item.
+    await tester.tap(find.byTooltip("Picker"));
+    // Trigger the frame.
+    await tester.pump();
 
-    // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    // Verify that the top bar has changed its title.
+    expect(find.text('Picker'), findsWidgets);
   });
 }
