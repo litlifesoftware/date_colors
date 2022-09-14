06fa3c436669e64f4fce92c25539c31b76762e0a
eightgran <eightgran@gmail.com>
2022-09-14 20:38:55 +0200

    Create basic integration test - Verify for correct color codes based on one example date

--- test/date_colors_test.dart
+++ test/date_colors_test.dart
@@ -1,7 +1,31 @@
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:date_colors/date_colors.dart';
 
 void main() {
-  // TODO: Integration Test
+  // Integration test.
+  DateTime date = DateTime(1983, DateTime.january, 1);
+  DateColorScheme scheme = DateColorScheme(date);
+
+  test('IT-01-CorrectColorOfTheYear', () {
+    expect(scheme.colorOfYear, const Color(0xffa1e8d9));
+  });
+
+  test('IT-02-CorrectColorOfTheSeason', () {
+    expect(scheme.colorOfTheSeason, const Color(0xff015696));
+  });
+
+  test('IT-03-CorrectCombinedColor', () {
+    expect(scheme.combinedColor, const Color(0xff519fb7));
+  });
+
+  test('IT-04-CorrectUniqueColorOfTheYear', () {
+    expect(scheme.uniqueColorOfTheYear, const Color(0xff1e4d36));
+  });
+
+  test('IT-05-CorrectDateColor', () {
+    expect(scheme.uniqueDateColor, const Color(0xfff86f0b));
+  });
 }
